2d16e295282546bff9d0c998c3f6ddb34d7297b2
Mesbaulhasanbsmrstu <mesbaulhasanbsmrstu@gmail.com>
2021-09-06 01:11:16 +0600

    yess

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component'; 
 import { ToastrModule } from 'ngx-toastr';
 import { SharedModule } from './custom_modules/shared.module';
+import { TestErrorComponent } from './errors/test-error/test-error.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { SharedModule } from './custom_modules/shared.module';
     MemberListComponent,
     MemberDetailComponent,
     ListsComponent,
-    MessagesComponent
+    MessagesComponent,
+    TestErrorComponent
   ],
   imports: [
     BrowserModule,
